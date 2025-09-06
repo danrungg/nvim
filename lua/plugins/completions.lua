186761955d17315c248b982d5e2f0ae6079e6c31
danrungg <git@daniel.cool>
2025-09-06 15:30:20 +0200

    trigger autocomplete with tab instead of enter

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -1,27 +1,26 @@
+-- lua/plugins/completions.lua
+
 return {
-  'saghen/blink.cmp',
-  dependencies = { 'rafamadriz/friendly-snippets' },
+	"saghen/blink.cmp",
+	dependencies = {
+		"hrsh7th/nvim-cmp",
+		"rafamadriz/friendly-snippets",
+	},
+	opts = {
+		keymap = {
+			preset = "default",
+			["<Tab>"] = { "accept" },
 
-  version = '1.*',
-  opts = {
-    keymap = {
-				preset = "default",
-				["<CR>"] = { "accept", "fallback" },
-				["<C><leader>"] = { "show" },
-			},
-    appearance = {
-      nerd_font_variant = 'mono'
-    },
-
-    completion = { documentation = { auto_show = true} },
-    sources = {
-      default = { 'lsp', 'path', 'snippets', 'buffer' },
-    },
-
-    fuzzy = { implementation = "prefer_rust_with_warning" }
-  },
-  opts_extend = { "sources.default" }
+			["<C><leader>"] = { "show" },
+		},
+		appearance = {
+			nerd_font_variant = "mono",
+		},
+		completion = { documentation = { auto_show = true } },
+		sources = {
+			default = { "lsp", "path", "snippets", "buffer" },
+		},
+		fuzzy = { implementation = "prefer_rust_with_warning" },
+	},
+	opts_extend = { "sources.default" },
 }
-
-
-
